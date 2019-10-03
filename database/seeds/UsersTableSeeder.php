f94c2c558cdbfe1f690c7009576a227c0d998f50
sheenazien <sheenazien08@gmail.com>
2019-10-03 21:50:25 +0700

    [AUTH] login

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create 10 users using the user factory
+        factory(App\Models\User::class, 10)->create();
+    }
+}

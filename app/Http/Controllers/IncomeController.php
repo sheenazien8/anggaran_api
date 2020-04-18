065dcb98e752797929e49f7ebf77572021f4f2e3
sheena <sheenazien08@gmail.com>
2020-04-19 06:06:35 +0700

    fix comment and try catch

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Income;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class IncomeController extends Controller
 {
+    /**
+     * Get All Data income by paginate 10
+     * @return Json Response
+     */
     public function index()
     {
         $incomes = Income::with('category', 'user')
-                            ->where('user_id', Cache::get('auth')->id)
+                            ->where('user_id', app('request')->auth->id)
                             ->orderBy('date', 'desc')->paginate(10);
 
         return response()->json([
@@ -20,11 +24,14 @@ class IncomeController extends Controller
             'message' => 'Success!'
         ], 200);
     }
-
+    /**
+     * Get All Income Data
+     * @return Reponse Json
+     */
     public function getAll()
     {
         $incomes = Income::with('category', 'user')
-                            ->where('user_id', Cache::get('auth')->id)
+                            ->where('user_id', app('request')->auth->id)
                             ->orWhereNull('user_id')->orderBy('date', 'desc')->get();
 
         return response()->json([
@@ -32,7 +39,11 @@ class IncomeController extends Controller
             'message' => 'Success!'
         ], 200);
     }
-
+    /**
+     * Get Detail Income by Id $income
+     * @param  Integer $income
+     * @return  Json Response
+     */
     public function detail($income)
     {
         $income = Income::with('category', 'user')->find($income);
@@ -42,58 +53,98 @@ class IncomeController extends Controller
             'income' => $income,
         ], 200);
     }
-
+    /**
+     * Store data income to database
+     * @param  Request $request [
+     *   'category_id', 'money', 'date
+     * ]
+     * @return Json Response
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
             'category_id' => 'required',
-            'money' => 'required',
+            'money' => 'required|int',
             'date' => 'required'
         ]);
-        $category = Category::find($request->category_id);
-        if ($category->type->name == 'Income') {
-            $income = new Income();
-            $income->fill($request->all());
-            $income->category()->associate($category);
-            $income->save();
-        }else {
+        try {
+            DB::beginTransaction();
+            $category = Category::find($request->category_id);
+            if ($category->type->name == 'Income') {
+                $income = new Income();
+                $income->fill($request->all());
+                $income->category()->associate($category);
+                $income->save();
+            } else {
+                DB::rollback();
+                return response()->json([
+                    "message" => "Category type is not allowed"
+                ], 402);
+            }
+            DB::commit();
+
             return response()->json([
-                "message" => "Category type is not allowed"
-            ], 402);
+                'message' => 'Success!',
+                'income' => $income,
+            ], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return response()->json([
+                'error' => true,
+                'message' => $e->message
+            ], 500);
         }
-
-        return response()->json([
-            'message' => 'Success!',
-            'income' => $income,
-        ], 200);
     }
-
+    /**
+     * Update data income
+     * @param  Request $request [
+     *   'category_id', 'money', 'date
+     * ]
+     * @param  Integer  $income
+     * @return Json Response
+     */
     public function update(Request $request, $income)
     {
         $income = Income::find($income);
         $this->validate($request, [
             'category_id' => 'required',
-            'money' => 'required',
+            'money' => 'requiredi|int',
             'date' => 'required'
         ]);
-        $category = Category::find($request->category_id);
-        if ($category->type->name == 'Income') {
-            $income->fill($request->all());
-            $income->category()->associate($category);
-            $income->save();
-        }else {
+        try {
+            DB::beginTransaction();
+            $category = Category::find($request->category_id);
+            if ($category->type->name == 'Income') {
+                $income->fill($request->all());
+                $income->category()->associate($category);
+                $income->save();
+            } else {
+                DB::rollback();
+                return response()->json([
+                    "message" => "Category type is not allowed"
+                ], 402);
+            }
+            DB::commit();
+
             return response()->json([
-                "message" => "Category type is not allowed"
-            ], 402);
+                'message' => 'Success!',
+                'income' => $income,
+            ], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return response()->json([
+                'error' => true,
+                'message' => $e->message
+            ], 500);
         }
-
-        return response()->json([
-            'message' => 'Success!',
-            'income' => $income,
-        ], 200);
     }
-
-
+    /**
+     * Delete data income
+     * @param  Integer $income
+     * @return Reponse Json
+     */
     public function delete($income)
     {
         $income = Income::find($income);
@@ -103,12 +154,16 @@ class IncomeController extends Controller
             'message' => 'Success!'
         ]);
     }
-
+    /**
+     * Search data income by description, user_id
+     * @param  Request $request
+     * @return Json Response
+     */
     public function search(Request $request)
     {
         $incomes = Income::with('category', 'user')
                             ->where('description', 'LIKE', "%%".$request->input('q')."%%")
-                            ->where('user_id', Cache::get('auth')->id)
+                            ->where('user_id', app('request')->auth->id)
                             ->orderBy('date', 'desc')->paginate(10);
 
         return response()->json([
